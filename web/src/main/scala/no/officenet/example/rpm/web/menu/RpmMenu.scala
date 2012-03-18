bfec185542b04f902d9d67325ae3f3578e27c357
Andreas Joseph Krogh <andreak@officenet.no>
2012-03-18 15:36:22 +0100

    Small code-cleanups

--- web/src/main/scala/no/officenet/example/rpm/web/menu/RpmMenu.scala
+++ web/src/main/scala/no/officenet/example/rpm/web/menu/RpmMenu.scala
@@ -5,7 +5,6 @@ import net.liftweb.sitemap.{Loc, Menu}
 object RpmMenu {
 	def menu = {
 		(Menu("Index") / "index" >> Loc.Hidden) ::
-		(Menu("Project-view wrapperForJSF") / "lift" / "project" / "projectViewWrapperForJSF" >> Loc.Hidden) ::
 		Menu(ProjectEditDialogWrapperLoc) ::
 		Menu(ProjectViewWrapperLoc) ::
 		Menu(ProjectListLoc, Menu(ProjectLoc)) ::
